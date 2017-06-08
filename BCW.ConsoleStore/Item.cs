e7cfd54314dcab821dae9d0f504c5a8eb62b6135
eriixon <radkovsn@gmail.com>
2017-06-08 13:39:19 -0600

    added item, store

---
+++ BCW.ConsoleStore/Item.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCW.ConsoleStore
+{
+    public class Item
+    {
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+
+        public Item(string name, string description, decimal price)
+        {
+            Name = name;
+            Description = description;
+            Price = price;
+        }
+    }
+}
---
+++ BCW.ConsoleStore/Store.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BCW.ConsoleStore
+{
+    class Store
+    {
+        public List<Item> items = new List<Item>();
+        public string Name { get; private set; }
+        public Store(string name)
+        {
+            Name = name;
+        }
+        // This method will allow you to add an item to the List of Items
+        public void AddItemToStore(Item item)
+        {
+            items.Add(item);
+        }
+
+        // This method should remove an item from the list of Items
+        public void RemoveItemFromStore(Item item)
+        {
+            items.Remove(item);
+        }
+    }
+}
